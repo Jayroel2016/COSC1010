6370ec318e43f4a5b81c58f2033e5a4b78311783
Luke <lukejayroe@westernwyoming.edu>
2025-03-02 23:15:07 +0000

    Ready for grading

--- Budget-Analysis/main.py
+++ Budget-Analysis/main.py
@@ -1,7 +1,26 @@
 #
-# Name
-# Date
+# Luke Jayroe
+# 3/2/25
 # Budget Analysis Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+# Establishing the budget
+
+userbudget = float( input ("Please enter how much you have budgeted for the month:" ))
+
+#variables
+
+total = 0
+
+another = 'y'
+
+#establishing the expenses taken from budget
+
+while another == 'y' or another == 'Y':
+    expense = float(input('Please enter the amount of your expense:'))
+    total += float(expense)
+    another = input('Do you want to enter another amount? enter y or Y for yes:')
+
+#finishing formula 
+
+print(userbudget - total)
