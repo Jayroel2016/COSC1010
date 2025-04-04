f1487d2b750f9cd2983abf1db5265e8618cc36ba
Luke <lukejayroe@westernwyoming.edu>
2025-04-04 23:03:49 +0000

    Ready for grading

--- File-Display/main.py
+++ File-Display/main.py
@@ -1,7 +1,19 @@
 #
-# Name
-# Date
+# Luke Jayroe
+# 4/4/25
 # File Display Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+# Opening the file
+myfile = open('numbers.txt', 'r') 
+
+# Read and display file's contents
+for line in myfile:
+    number = int(line)
+    print(number)
+
+# Close File
+myfile.close()
+
+
+# Error message saying i do not have the numbers.txt file.
