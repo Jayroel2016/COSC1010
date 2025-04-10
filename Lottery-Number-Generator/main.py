ccf52437bc909cf34049b112055d8019ac60093d
Luke <lukejayroe@westernwyoming.edu>
2025-04-10 01:07:07 +0000

    Ready For Grading

--- Lottery-Number-Generator/main.py
+++ Lottery-Number-Generator/main.py
@@ -1,7 +1,29 @@
 #
-# Name
-# Date
+# Luke Jayroe
+# 4/9/25
 # Lottery Number Generator Programming Project
 # COSC 1010
 #
-# Use comments liberally throughout the program. 
+import random
+
+MAX_DIGITS = 7
+START = 0
+END = 9
+
+# Main function
+def main ():
+    # Making list
+    numbers = [0] * 7
+
+    # Populating list with random numbers
+    for index in range (MAX_DIGITS) :
+        numbers[index] = random.randint (START, END)
+
+    # Display random list
+    print ('Here are your lottery numbers:')
+    for index in range (MAX_DIGITS) :
+        print(numbers[index], end='')
+        print()
+
+# Call main function
+main ()
